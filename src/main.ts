40da6b2244bc859e58ef7f6c3a77a6d49bf207a9
szymonaszek01 <235879@edu.p.lodz.pl>
2024-05-31 00:25:26 +0200

    updating global styles and assets

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(AppComponent, {providers: []}).catch(error => console.error(error));
+bootstrapApplication(AppComponent, {providers: [provideRouter(appRoutes), provideAnimationsAsync()]}).catch(error => console.error(error));
