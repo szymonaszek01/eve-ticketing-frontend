b8c54987767bf3d7c545f8a7836ab19db817c10c
szymonaszek01 <235879@edu.p.lodz.pl>
2024-06-11 11:39:12 +0200

    adding footer component

---
+++ src/app/public/shared/components/footer/footer.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { NgForOf, NgOptimizedImage } from '@angular/common';
+import { RoundedIconComponent } from '../../../../shared/shared/components/rounded-icon/rounded-icon.component';
+import { MatListItem } from '@angular/material/list';
+import { SideBarRoute } from '../../models/side-bar-route';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+    RoundedIconComponent,
+    MatListItem,
+    NgForOf
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  @Input()
+  public routeList: SideBarRoute[];
+
+  constructor() {
+    this.routeList = [];
+  }
+}
