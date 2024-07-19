49993737fca214a231e706d10a7d1175ec2a2e47
szymonaszek01 <235879@edu.p.lodz.pl>
2024-07-19 20:25:58 +0200

    Updating ticket create component

--- src/app/private/ticket/features/ticket-create/ticket-create.component.ts
+++ src/app/private/ticket/features/ticket-create/ticket-create.component.ts
@@ -8,15 +8,16 @@ import { AuthState } from '../../../../public/auth/models/auth-state';
 import { selectAuth } from '../../../../public/auth/data-access/auth-reducers';
 import { User } from '../../../../shared/shared/models/user';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { renderApiError, renderError } from '../../../../shared/shared/util/util';
+import { renderApiError, renderError, ticketReservedFilter } from '../../../../shared/shared/util/util';
 import { ApiError } from '../../../../shared/shared/models/api-error';
 import { MatButton } from '@angular/material/button';
 import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
 import { NgForOf, NgIf } from '@angular/common';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import { selectError } from '../../data-access/ticket-reducers';
+import { selectError, selectLastAdded, selectLastUpdated } from '../../data-access/ticket-reducers';
 import { MatIcon } from '@angular/material/icon';
+import { Ticket } from '../../models/ticket';
 
 @Component({
   selector: 'app-ticket-create',
@@ -50,29 +51,54 @@ export class TicketCreateComponent {
 
   private user: User | undefined;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Event,
+  private readonly ticket: Ticket | undefined;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
               private dialogRef: MatDialogRef<TicketCreateComponent>,
               private formBuilder: FormBuilder,
               private ticketStore: Store<{ ticket: TicketState }>,
               private authStore: Store<{ auth: AuthState }>,
   ) {
-    this.event = data;
+    if ('event' in data) {
+      this.event = data.event;
+    }
+    if ('ticket' in data) {
+      this.ticket = data.ticket;
+      this.event = data.ticket.event;
+    }
     this.formGroup = this.formBuilder.group({
-      firstname: ['', [Validators.required]],
-      lastname: ['', [Validators.required]],
-      phoneNumber: ['', [Validators.required]],
-      isAdult: [false, Validators.required],
-      isStudent: [false, Validators.required]
+      firstname: [this.ticket ? this.ticket.firstname : '', this.ticket ? [] : [Validators.required]],
+      lastname: [this.ticket ? this.ticket?.lastname : '', this.ticket ? [] : [Validators.required]],
+      phoneNumber: [this.ticket ? this.ticket?.phoneNumber : '', this.ticket ? [] : [Validators.required]],
+      isAdult: [this.ticket ? this.ticket?.isAdult ? 'true' : 'false' : '', this.ticket ? [] : [Validators.required]],
+      isStudent: [this.ticket ? this.ticket?.isStudent ? 'true' : 'false' : '', this.ticket ? [] : [Validators.required]]
     });
   }
 
   ngOnInit(): void {
-    this.ticketStore.select(selectError).subscribe(apiError => {
-      if (this.formGroup.valid && !apiError) {
+    this.ticketStore.select(selectLastAdded).subscribe(ticket => {
+      if (this.user && ticket) {
+        this.ticketStore.dispatch(ticketActions.loadReserved({
+          page: 0,
+          size: 5,
+          filter: ticketReservedFilter(this.user.id),
+          sort: undefined
+        }));
         this.dialogRef.close();
       }
-      this.apiError = apiError;
     });
+    this.ticketStore.select(selectLastUpdated).subscribe(ticket => {
+      if (this.user && ticket) {
+        this.ticketStore.dispatch(ticketActions.loadReserved({
+          page: 0,
+          size: 5,
+          filter: ticketReservedFilter(this.user.id),
+          sort: undefined
+        }));
+        this.dialogRef.close();
+      }
+    });
+    this.ticketStore.select(selectError).subscribe(apiError => this.apiError = apiError);
     this.authStore.select(selectAuth).subscribe(auth => this.user = auth);
   }
 
@@ -80,24 +106,31 @@ export class TicketCreateComponent {
     return key ? renderError(this.formGroup.controls[key], key) : renderApiError(this.apiError);
   }
 
-  protected onInput(): void {
-    if (this.apiError) {
-      this.ticketStore.dispatch(ticketActions.loadError({error: undefined}));
-    }
-  }
-
   protected onSubmit(): void {
     if (!this.formGroup.valid || !this.event || !this.user) {
       return;
     }
-    this.ticketStore.dispatch(ticketActions.add({
-      ticketReq: {
-        ...this.formGroup.value,
-        isAdult: this.formGroup.value.isAdult === 'true',
-        isStudent: this.formGroup.value.isStudent === 'true',
-        eventId: this.event.id,
-        userId: this.user.id
+    if (this.apiError) {
+      this.ticketStore.dispatch(ticketActions.loadError({error: undefined}));
+    }
+    if (this.ticket) {
+      const body: any = {id: this.ticket.id};
+      for (const key in this.formGroup.value) {
+        if (this.formGroup.value.hasOwnProperty(key) && this.formGroup.value[key]) {
+          body[key] = (key === 'isAdult' || key === 'isStudent') ? this.formGroup.value[key] === 'true' : this.formGroup.value[key];
+        }
       }
-    }));
+      this.ticketStore.dispatch(ticketActions.update({values: body}));
+    } else {
+      this.ticketStore.dispatch(ticketActions.add({
+        ticketReq: {
+          ...this.formGroup.value,
+          isAdult: this.formGroup.value.isAdult === 'true',
+          isStudent: this.formGroup.value.isStudent === 'true',
+          eventId: this.event.id,
+          userId: this.user.id
+        }
+      }));
+    }
   }
 }
